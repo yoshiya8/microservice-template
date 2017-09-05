18ae42a9814bf663559901b2c91b99eca7f99430
Joshua Haftel <jhaftel@gmail.com>
2017-09-05 14:04:18 -0400

    Added swagger

--- src/main/java/com/vencore/microservicetemplate/MicroserviceTemplateApplication.java
+++ src/main/java/com/vencore/microservicetemplate/MicroserviceTemplateApplication.java
@@ -8,6 +8,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
 @SpringBootApplication
 @EnableMongoRepositories
 public class MicroserviceTemplateApplication {
@@ -20,6 +25,12 @@ public class MicroserviceTemplateApplication {
             }
         };
     }
+
+	@Bean
+	public Docket swaggerApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceTemplateApplication.class, args);
